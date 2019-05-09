9dce0ebb0b1ce1c5a15e939e2839089de4621605
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-05-09 12:45:23 +1000

    linting fix

--- src/flag_table.rs
+++ src/flag_table.rs
@@ -1,5 +1,5 @@
-use std::collections::HashMap;
 use crate::event::event_hash::EventHash;
+use std::collections::HashMap;
 
 // FlagTable is a map from EventHash into Frame number (u64)
 pub type FlagTable = HashMap<EventHash, u64>;
--- src/store/store.rs
+++ src/store/store.rs
@@ -1,21 +1,26 @@
-use crate::Event;
 use crate::event::event_hash::EventHash;
-use crate::PeerId;
+use crate::Event;
 use crate::FlagTable;
+use crate::PeerId;
 use failure::Error;
 
 // Store trait for Poset
 pub trait Store {
-    fn topological_events(&self, start, finish: u64) -> Result<Vec<Event>, Error>;
-    fn set_event(&self, ev: Event) -> Result<(),Error>;
+    fn topological_events(&self, start: u64, finish: u64) -> Result<Vec<Event>, Error>;
+    fn set_event(&self, ev: Event) -> Result<(), Error>;
     fn get_event(&self, hash: EventHash) -> Result<Event, Error>;
     fn get_store_path(&self) -> Result<String, Error>;
     fn get_clotho_check(&self, frame: u64, hash: EventHash) -> Result<EventHash, Error>;
     fn get_clotho_creator_check(&self, frame: u64, ctreator: PeerId) -> Result<EventHash, Error>;
     fn add_clotho_check(&self, frame: u64, creator: PeerID, hash: EventHash) -> Result<(), Error>;
-    fn add_time_table(&self, to: EventHash, from: EventHash, lamport_time: u64) -> Result<(), Error>;
+    fn add_time_table(
+        &self,
+        to: EventHash,
+        from: EventHash,
+        lamport_time: u64,
+    ) -> Result<(), Error>;
     fn get_time_table(&self, hash: EventHash) -> Result<FlagTable, Error>;
     fn check_frame_finality(&self, frame: u64) -> bool;
     // process_out_frame() uses for debug/tracking/research purposes to dump frame when it's finalised
-    fn process_out_frame(&self, frame: u64, address: String) ->Result<(), Error>;
+    fn process_out_frame(&self, frame: u64, address: String) -> Result<(), Error>;
 }
